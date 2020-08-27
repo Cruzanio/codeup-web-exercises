09a9691569253cef8dd1f96cf5f1ff0449df5db7
Cruzanio <cruzanio.villarreal@gmail.com>
2020-08-27 11:13:46 -0500

    Markers added

--- js/maps.js
+++ js/maps.js
@@ -3,7 +3,13 @@
 mapboxgl.accessToken = MAPBOX_TOKEN;
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    style: 'mapbox://styles/mapbox/satellite-streets-v11', // stylesheet location
+    center: [-98.4916, 29.4252], // starting position [lng, lat]
+    zoom: 17// starting zoom
 });
+
+//Geekdom
+var marker = new mapboxgl.Marker().setLngLat([-98.4935, 29.4264]).addTo(map)
+
+//Codeup
+var marker1 = new mapboxgl.Marker().setLngLat([-98.4896, 29.4268]).addTo(map)
