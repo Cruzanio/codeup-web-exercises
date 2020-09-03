da624bd9f32009fb4464b2e2cb451c2c6be99292
Cruzanio <cruzanio.villarreal@gmail.com>
2020-09-03 17:30:45 -0500

    Hiding Banners

--- js/konami.js
+++ js/konami.js
@@ -16,6 +16,7 @@ setInterval(() => {
 $('#bg-img').css('background-image', "url('img/Undertale-Background.jpg')")
 $('#success-banner').hide()
 $('#fail-banner').hide()
+$('#fail').hide()
 
 var cheatEnter = ''
 var fail = new Audio('img/evil-laugh.mp3')
@@ -25,10 +26,17 @@ $(document).keyup(function () {
     var code = '3838404037393739666513'
     cheatEnter += event.keyCode
     if (cheatEnter === code) {
+        $('#question').hide()
+        $('#cursor').hide()
         success.play()
         $('#success-banner').show()
         cheatEnter = ''
     } else if (cheatEnter.length > 22) {
+        $('#cursor').hide()
+
+        $('#start').hide()
+        $('#success-banner').hide()
+        $('#fail').show()
         $('#bg-img').css('background-image', "url('img/fail-background.jpg')")
         fail.play()
         $('#fail-banner').show()
