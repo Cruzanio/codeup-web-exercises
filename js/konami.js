09e20ffb81aa8b2084dfb3f173669fc0a053fbab
Cruzanio <cruzanio.villarreal@gmail.com>
2020-09-03 16:50:08 -0500

    Separated Konami CSS/HTML/JS

---
+++ js/konami.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var cursor = true;
+var speed = 220;
+
+setInterval(() => {
+    if(cursor) {
+        document.getElementById('cursor').style.opacity = 0;
+        cursor = false;
+    }else {
+        document.getElementById('cursor').style.opacity = 1;
+        cursor = true;
+    }
+}, speed);
+
+$('#bg-img').css('background-image', "url('img/Undertale-Background.jpg')")
+$('#success-banner').hide()
+$('#fail-banner').hide()
+
+var cheatEnter = ''
+var fail = new Audio('img/evil-laugh.mp3')
+var success = new Audio('img/ta-da.mp3')
+
+$(document).keyup(function () {
+    var code = '3838404037393739666513'
+    cheatEnter += event.keyCode
+    if (cheatEnter === code) {
+        success.play()
+        $('#success-banner').show()
+        cheatEnter = ''
+    } else if (cheatEnter.length > 22) {
+        $('#bg-img').css('background-image', "url('img/fail-background.jpg')")
+        fail.play()
+        $('#fail-banner').show()
+        cheatEnter = ''
+    }
+});
