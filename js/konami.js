f3d274e28a7af88b298f6c4c7d5fa497a2c4d8c9
Cruzanio <cruzanio.villarreal@gmail.com>
2020-09-04 09:17:08 -0500

    Refactored check into for loop array

--- js/konami.js
+++ js/konami.js
@@ -18,28 +18,32 @@ $('#success-banner').hide()
 $('#fail-banner').hide()
 $('#fail').hide()
 
-var cheatEnter = ''
+var cheatEnter = []
 var fail = new Audio('img/evil-laugh.mp3')
 var success = new Audio('img/ta-da.mp3')
 
 $(document).keyup(function () {
-    var code = '3838404037393739666513'
-    cheatEnter += event.keyCode
-    if (cheatEnter === code) {
-        $('#question').hide()
-        $('#cursor').hide()
-        success.play()
-        $('#success-banner').show()
-        cheatEnter = ''
-    } else if (cheatEnter.length > 22) {
-        $('#cursor').hide()
-
-        $('#start').hide()
-        $('#success-banner').hide()
-        $('#fail').show()
-        $('#bg-img').css('background-image', "url('img/fail-background.jpg')")
-        fail.play()
-        $('#fail-banner').show()
-        cheatEnter = ''
+    var konamiArray = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13]
+    cheatEnter.push(event.keyCode)
+    for (var i=0; i <= cheatEnter.length-1; i++) {
+        if (cheatEnter.length <= 11) {
+            if (JSON.stringify(cheatEnter) === JSON.stringify(konamiArray)) {
+                $('#question').hide()
+                $('#cursor').hide()
+                success.play()
+                $('#success-banner').show()
+                cheatEnter = []
+            }
+        } else {
+            $('#question').hide()
+            $('#cursor').hide()
+            $('#start').hide()
+            $('#success-banner').hide()
+            $('#fail').show()
+            $('#bg-img').css('background-image', "url('img/fail-background.jpg')")
+            fail.play()
+            $('#fail-banner').show()
+            cheatEnter = []
+        }
     }
 });
