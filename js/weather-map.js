b60ca157ee976675d43b745cb57462349c28d6ad
Cruzanio <cruzanio.villarreal@gmail.com>
2020-09-10 23:24:42 -0500

    functional 7 days

--- js/weather-map.js
+++ js/weather-map.js
@@ -11,7 +11,7 @@
                     $('#bg').css('background-image', "url(img/rain.jpg)")
                     $('#icon').html('<img class="image mt-5" src="img/rain-icon.png">')
                 } else if (data.current.weather[0].main === 'Clouds') {
-                        $('#bg').css('background-image', "url(img/cloudy-bg.jpeg)")
+                    $('#bg').css('background-image', "url(img/cloudy-bg.jpeg)")
                     $('#icon').html('<img class="image mt-5" src="img/cloudy.png">')
                     $('.card1').css('color', 'black')
                 } else if (data.current.weather[0].main === 'Sun') {
@@ -21,7 +21,7 @@
                 $('#degrees').html(Math.round(data.current.temp) + '&#176')
                 var unix = data.current.dt * 1000
                 var date = new Date(unix)
-                $('#date').html( dayInput(date.getDay()) + ' ' + date.getDate() + ', ' + date.getFullYear() + '<br>\n' +
+                $('#date').html(dayInput(date.getDay()) + ' ' + date.getDate() + ', ' + date.getFullYear() + '<br>\n' +
                     'Last Updated: ' + date.getHours() + ':' + date.getMinutes())
                 $('#cloudy').html(data.current.clouds + '%')
                 $('#humidity').html(data.current.humidity + '%')
@@ -32,34 +32,53 @@
         mapLoad()
 
 
-        var newDay = function (dayOfTheWeek) {
-            for (var i = 0; i <= data.daily.length-1; i++)
-            $('#cityName').html('San Antonio')
-            $('#weatherType').html(data.daily[0].weather[0].description)
-            if (data.daily[0].weather[0].description === 'light rain') {
-                $('#bg').css('background-image', "url(img/rain.jpg)")
-                $('#icon').html('<img class="image mt-5" src="img/rain-icon.png">')
-            } else if (data.daily[0].weather[0].description === 'cloudy') {
-                $('#bg').css('background-image', "url(img/cloudy-bg.jpeg)")
-                $('#icon').html('<img class="image mt-5" src="img/cloudy.png">')
-            } else if (data.daily[0].weather[0].description === 'sunny') {
-                $('#bg').css('background-image', "url(img/xp.jpg)")
-                $('#icon').html('<img class="image mt-5" src="img/sun.png">')
+        var newDay = function (day) {
+            var day1 = day
+            $.get("https://api.openweathermap.org/data/2.5/onecall?lat=29.424122&lon=-98.493629&exclude=hourly,minutely&appid=" + OPEN_WEATHER_MAP_API + "&units=imperial"
+            ).done(function (data) {
+                console.log(data)
+            for (var i = 0; i <= data.daily.length - 1; i++) {
+                console.log('here')
+                var unix = data.daily[i].dt * 1000
+                var date = new Date(unix)
+                console.log(dayInput(date.getDay()))
+                console.log(day1)
+                if ((dayInput(date.getDay()) === day1)) {
+                    $('#weatherType').html(data.daily[i].weather[0].description)
+                    if (data.daily[i].weather[0].main === 'Rain') {
+                        $('#bg').css('background-image', "url(img/rain.jpg)")
+                        $('#icon').html('<img class="image mt-5" src="img/rain-icon.png">')
+                        $('.card1').css('color', 'white')
+                    } else if (data.daily[i].weather[0].main === 'Clouds') {
+                        $('#bg').css('background-image', "url(img/cloudy-bg.jpeg)")
+                        $('#icon').html('<img class="image mt-5" src="img/cloudy.png">')
+                        $('.card1').css('color', 'black')
+                    } else if (data.daily[i].weather[0].main === 'Clear') {
+                        $('#bg').css('background-image', "url(img/xp.jpg)")
+                        $('#icon').html('<img class="image mt-5" src="img/sun.png">')
+                    }
+                    $('#degrees').html(Math.round(data.daily[i].temp.max) + '&#176')
+                    $('#date').html(dayInput(date.getDay()) + ' ' + date.getDate() + ', ' + date.getFullYear() + '<br>\n' +
+                        'Last Updated: ' + date.getHours() + ':' + date.getMinutes())
+                    $('#cloudy').html(data.daily[i].clouds + '%')
+                    $('#humidity').html(data.daily[i].humidity + '%')
+                    $('#wind').html(data.daily[i].wind_speed + ' km/h')
+                    $('#rain').html(data.daily[i].rain + ' mm')
+                }
             }
-            $('#degrees').html(Math.round(data.current.temp) + '&#176')
-            var unix = data.current.dt * 1000
-            var date = new Date(unix)
-            $('#date').html( dayInput(date.getDay()) + ' ' + (date.getMonth()+1) + ', ' + date.getFullYear() + '<br>\n' +
-                'Last Updated: ' + date.getHours() + ':' + date.getMinutes())
-            $('#cloudy').html(data.daily[0].clouds + '%')
-            $('#humidity').html(data.daily[0].humidity + '%')
-            $('#wind').html(data.daily[0].wind_speed + ' km/h')
-            $('#rain').html(data.daily[0].rain + ' mm')
+            $('#cityName').html('San Antonio')
+            })
         }
+
+        $('.dayOfTheWeek').click(function () {
+            var dayOut = ($(this).html())
+            newDay(dayOut)
+        })
 
         $('#update').click(function () {
             mapLoad()
         })
+
         function dayInput(input) {
             switch (input) {
                 case 0:
@@ -95,7 +114,6 @@
         center: [-98.493629, 29.424122], // starting position [lng, lat]
         zoom: 12// starting zoom
     });
-
 
 })();
 
