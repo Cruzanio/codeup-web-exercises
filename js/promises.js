f184fec52459fcc77af535694cf7cbb73daa1ece
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-09-21 10:35:27 -0500

    All promise

--- js/promises.js
+++ js/promises.js
@@ -1,8 +1,20 @@
-fetch("https://swapi.dev/api/planets/6/")
+let starFetch = fetch("https://swapi.dev/api/planets/6/")
     .then(result => result.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        return "Star Wars complete"
+    })
 
-fetch("https://anapioficeandfire.com/api/houses/578")
+let GOTfetch = fetch("https://anapioficeandfire.com/api/houses/378")
     .then(result => result.json())
     .catch(error => console.log(error))
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        return "GOT complete"
+    })
+
+Promise.all([starFetch, GOTfetch])
+.then(data => {
+    console.log('Both are complete!')
+    console.log(data);
+})
