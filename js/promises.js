8f80a1c8bdbc36324f8008723a4072894eb4804c
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-09-21 10:43:19 -0500

    Turtle race

--- js/promises.js
+++ js/promises.js
@@ -18,3 +18,35 @@ Promise.all([starFetch, GOTfetch])
     console.log('Both are complete!')
     console.log(data);
 })
+
+let redTurtle = Math.floor(Math.random()*10000)
+let blueTurtle = Math.floor(Math.random()*10000)
+let orangeTurtle = Math.floor(Math.random()*10000)
+let purpleTurtle = Math.floor(Math.random()*10000)
+
+let goRedTurtle = new Promise((resolve => {
+    setTimeout(() => {
+        resolve("Red Turtle Finished!")
+    }, redTurtle)
+}));
+
+let goBlueTurtle = new Promise((resolve => {
+    setTimeout(() => {
+        resolve("Blue Turtle Finished!")
+    }, blueTurtle)
+}));
+
+let goOrangeTurtle = new Promise((resolve => {
+    setTimeout(() => {
+        resolve("Orange Turtle Finished!")
+    }, orangeTurtle)
+}));
+
+let goPurpleTurtle = new Promise((resolve => {
+    setTimeout(() => {
+        resolve("Purple Turtle Finished!")
+    }, purpleTurtle)
+}));
+
+Promise.race([goRedTurtle, goBlueTurtle, goOrangeTurtle, goPurpleTurtle])
+.then(data => console.log(data))
